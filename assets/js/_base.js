8ce1157cc1c67f7d8093555c1454141ff9066800
lemuel <lemuel@leetdigital.com>
2018-03-24 18:42:33 +0800

    first commit

---
+++ assets/js/_base.js
@@ -0,0 +1,8 @@
+// BASE FRAMEWORK IMPORT
+// import all global framework scripts and dependencies here:
+// @codekit-prepend '../bower_components/jquery/dist/jquery.js'
+// @codekit-prepend '../bower_components/uikit/js/uikit.js'
+
+
+// GLOBAL FUNCTIONS
+// define global functions here:
---
+++ assets/js/error.js
@@ -0,0 +1,21 @@
+// BASE FILE IMPORT
+// @codekit-prepend '_base.js'
+
+// PAGE FRAMEWORK COMPONENT IMPORT
+// import any framework components that are used in this page only:
+// @codekit-prepend '../bower_components/uikit/js/components/slider.js'
+// @codekit-prepend '../bower_components/uikit/js/components/form-file.js'
+
+
+
+// PAGE FUNCTIONS
+// define needed page functions here, when document.readyState === "complete":
+
+
+(function($) {
+    
+    /* page functions go here */
+ 
+ 
+})(jQuery);
+
